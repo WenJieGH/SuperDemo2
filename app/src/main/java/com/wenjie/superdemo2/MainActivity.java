6d8a102d9ee019ba9e3cd9c6ebcb832bdab19d16
WenJie <734957649@qq.com>
2016-05-27 11:56:28 +0800

    双击退出程序显示吐司 温杰

--- app/src/main/java/com/wenjie/superdemo2/MainActivity.java
+++ app/src/main/java/com/wenjie/superdemo2/MainActivity.java
@@ -2,6 +2,7 @@ package com.wenjie.superdemo2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private long lastTime;
+
+    public void onBackPressed(){
+        long ct = System.currentTimeMillis();
+        if(ct-lastTime>2000){
+            Toast.makeText(this,"再次点击后退键退出",Toast.LENGTH_SHORT).show();
+            lastTime=ct;
+        }else {
+            super.onBackPressed();
+        }
     }
 }
